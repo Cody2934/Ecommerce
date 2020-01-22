1ef8368ae8a9175fa96887382db7ce426aff3a00
Cody Brown <cody6lefever@gmail.com>
2020-01-21 20:14:39 -0800

    got basic data to show but need more

--- src/renderCars.js
+++ src/renderCars.js
@@ -7,20 +7,8 @@ function renderCars(cars) {
     h3.textContent = cars.name;
     li.appendChild(h3);
 
-    // const img = document.createElement('img');
-    // img.src = cars.image;
-    // img.alt = cars.name + ' image';
-    // li.appendChild(img);
-
     const p = document.createElement('p');
     p.className = 'price';
-
-    const usd = '$' + cars.price.toFixed(2);
-    // const usd = cars.price.toLocaleString('en-US', { 
-    //     style: 'currency', 
-    //     currency: 'USD' 
-    // });
-    p.textContent = usd;
     
     const button = document.createElement('button');
     button.textContent = 'Add';
@@ -31,5 +19,7 @@ function renderCars(cars) {
 
     return li;
 }
+
+
 
 export default renderCars;
