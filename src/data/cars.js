41ce651bf226f91540059fef0605f7fe2ba61cb4
Cody Brown <cody6lefever@gmail.com>
2020-01-22 11:08:46 -0800

    all code works and ready for next step

--- src/data/cars.js
+++ src/data/cars.js
@@ -2,63 +2,64 @@ const camaro = {
     id: 'camaro',
     name: 'Chevrolet Camaro SS',
     image: 'https://o.aolcdn.com/images/dims3/GLOB/crop/4869x2739+0+0/resize/800x450!/format/jpg/quality/85/http://o.aolcdn.com/hss/storage/midas/53169a7326818af49592875acb092e23/206481613/2018ChevyCamaroSS1LE_1.jpg',
-    description: 'Rear Wheel Drive V8 good for rednecks and track enthusiasts',
+    description: 'The sister kissin cousin lover car of Murica!',
     category: 'RWD',
-    price: 25000
+    price: 25
 };
 
 const sti = {
     id: 'sti',
     name: 'Subaru Impreza WRX STi',
     image: 'https://images.freeimages.com/images/small-previews/ac9/impreza-1-1557545.jpg',
-    description: 'All Wheel Drive turbo 4cyl good for street races and vapers',
+    description: '#VapeLife #HeadgasketBlewAgain #LowOnOil',
     category: 'AWD',
-    price: 30000
+    price: 30
 };
 
 const mini = {
     id: 'mini',
-    name: 'Mini Cooper S JCW',
+    name: 'Mini Cooper S JCW GP',
     image: 'https://media.fastestlaps.com/mini-cooper-jcw-gp.jpg',
-    description: 'Easiest to park corner hugger',
+    description: 'For the NoT sO nOrMaL',
     category: 'FWD',
-    price: 33000
+    price: 33
 };
 
 const civic = {
     id: 'civic',
     name: 'Honda Civic Type R',
     image: 'https://www.driving.co.uk/s3/st-driving-prod/uploads/2017/11/civic-09.jpg',
-    description: 'Kids love them as they have fart cannons',
+    description: 'JDM Fanbois Unite for the King Has Returned',
     category: 'FWD',
-    price: 36000
+    price: 36
+};
+
+
+const evo = {
+    id: 'evo',
+    name: 'Mitsubishi MR',
+    image: 'https://cdn11.bigcommerce.com/s-jyx4srauta/images/stencil/1280x1280/products/151/1425/evox2__66130.1551900094.jpg',
+    description: 'The DarkSide of the Force',
+    category: 'AWD',
+    price: 40
+};
+
+const gtr = {
+    id: 'gtr',
+    name: 'Nissan GTR Nissmo',
+    image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/my20-gt-r-nismo-germany-24-source-1562607770.jpg',
+    description: 'AAAAAHHHHH!!!!! GODZILLA!!!',
+    category: 'AWD',
+    price: 113
 };
 
 const corvette = {
     id: 'corvette',
     name: 'Chevrolet Corvette ZR1',
     image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2019-chevrolet-corvette-zr1-placement-test-1527205487.jpg',
-    description: 'The white shoes blue jeans old midlife crisis machine',
+    description: 'Must be in a midlife crisis and own jean shorts',
     category: 'RWD',
-    price: 120000
-};
-
-const evo = {
-    id: 'evo',
-    name: 'Mitsubishi MR',
-    image: 'https://cdn11.bigcommerce.com/s-jyx4srauta/images/stencil/1280x1280/products/151/1425/evox2__66130.1551900094.jpg',
-    description: 'If you love the darkside and hate subaurs',
-    category: 'AWD',
-    price: 40000
-};
-
-const gtr = {
-    id: 'gtr',
-    name: 'Nissan GTR',
-    image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/my20-gt-r-nismo-germany-24-source-1562607770.jpg',
-    description: 'Refered to Godzilla by kids on the internet',
-    category: 'AWD',
-    price: 113000
+    price: 120
 };
 
 const rides = [
@@ -66,9 +67,9 @@ const rides = [
     sti, 
     mini, 
     civic, 
-    corvette, 
     evo, 
-    gtr
+    gtr,
+    corvette 
 ];
 
 export default rides;
