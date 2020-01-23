57da6d937732eaf86ebdd174b081fd6d8d4a017e
Cody Brown <cody6lefever@gmail.com>
2020-01-23 10:53:51 -0800

    actually works now

--- shoppingcart/shoppingcart.js
+++ shoppingcart/shoppingcart.js
@@ -1,5 +1,5 @@
 import cart from '../data/cart.js';
-import fruits from '../data/cars.js';
+import rides from '../data/cars.js';
 import { findById, calcOrderTotal, toUSD } from '../common/utils.js';
 import renderLineItem from './render-line-item.js';
 
@@ -8,11 +8,11 @@ const orderTotalCell = document.getElementById('order-total-cell');
 
 for (let i = 0; i < cart.length; i++) {
     const lineItem = cart[i];
-    const cars = findById(fruits, lineItem.id);
-    const dom = renderLineItem(lineItem, cars);
+    const findCars = findById(rides, lineItem.id);
+    const dom = renderLineItem(lineItem, findCars);
 
     tbody.appendChild(dom);
 }
 
-const orderTotal = calcOrderTotal(cart, cars);
+const orderTotal = calcOrderTotal(cart, rides);
 orderTotalCell.textContent = toUSD(orderTotal);
