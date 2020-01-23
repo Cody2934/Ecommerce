857c44fc9ffe510db694aa984a84e740e57acc40
Cody Brown <cody6lefever@gmail.com>
2020-01-22 16:49:04 -0800

    created cart data literals

---
+++ src/data/cart.js
@@ -0,0 +1,24 @@
+const cart = [{
+    id: 'camaro',
+    quantity: 7
+}, {
+    id: 'sti',
+    quantity: 6
+}, { 
+    id: 'mini',
+    quantity: 5
+}, {
+    id: 'civic',
+    quantity: 4
+}, {
+    id: 'evo',
+    quantity: 3
+}, {
+    id: 'gtr',
+    quantity: 2
+}, {
+    id: 'corvette',
+    quantity: 1
+}];
+
+export default cart;
